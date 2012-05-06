3e02e599ae18ab1fe23847d00ce7450a78024f94
shell@debox <shell909090@gmail.com>
2012-05-06 11:21:57 +0800

    * 修改windows下字体配置

--- emacs-win.el
+++ emacs-win.el
@@ -4,12 +4,14 @@
 ;; maximize
 (run-with-idle-timer 1 nil 'w32-send-sys-command 61488)
 
-(custom-set-faces
- '(default
-    ((t (:inherit nil :stipple nil :background "SystemWindow"
-		  :foreground "SystemWindowText" :inverse-video nil
-		  :box nil :strike-through nil :overline nil :underline nil
-		  :slant normal :weight normal :height 105 :width normal
-		  :foundry "outline" :family "Courier New")))))
+;; Setting English Font
+(set-face-attribute
+  'default nil :font "Consolas 12")
+ 
+;; Chinese Font
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset
+                      (font-spec :family "Microsoft Yahei" :size 12)))
 
 (setq popup-terminal-command '("cmd" "/c" "start"))
