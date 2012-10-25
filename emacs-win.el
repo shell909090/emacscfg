4dddd3c3c7e82f6c848c044a93ffe428c627cd63
shell909090@debox <shell909090@gmail.com>
2012-10-25 22:26:22 +0800

    * 调整windows下的字体

--- emacs-win.el
+++ emacs-win.el
@@ -12,6 +12,6 @@
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font)
                       charset
-                      (font-spec :family "Microsoft Yahei" :size 12)))
+                      (font-spec :family "Microsoft Yahei" :size 15)))
 
 ;;; emacs-win.el ends here
