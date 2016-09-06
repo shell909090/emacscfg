522655a4bdf7024dc49e984fd72f99b363686e3e
Shell.Xu <shell909090@gmail.com>
2016-09-06 15:51:46 +0800

    + add ein

---
+++ elpa/request-20160822.1659/request-autoloads.el
@@ -0,0 +1,15 @@
+;;; request-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("request.el") (22478 26985 185245 559000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; request-autoloads.el ends here
