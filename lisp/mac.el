21ccdaf0ffabe7bbec3e58623ad0e5a6b2adf5b7
Shell.Xu <shell909090@gmail.com>
2018-07-20 14:44:49 +0800

    little fix, tested under mac.

--- lisp/mac.el
+++ lisp/mac.el
@@ -1,8 +1,19 @@
 ;;; mac.el --- 
 (server-start)
 
-(set-frame-font "Noto Sans Mono CJK SC-14") ;; setup font size
-(add-to-list 'default-frame-alist '(font .  "Noto Sans Mono CJK SC-14")) ;; for multi frame
+(set-frame-font "Monaco-16") ;; setup font size
+(add-to-list 'default-frame-alist '(font .  "Monaco-16")) ;; for multi frame
+
+;; default Latin font (e.g. Consolas)
+(set-face-attribute 'default nil :family "Monaco")
+
+;; default font size (point * 10)
+;;
+;; WARNING!  Depending on the default font,
+;; if the size is not supported very well, the frame will be clipped
+;; so that the beginning of the buffer may not be visible correctly. 
+(set-face-attribute 'default nil :height 160)
+
 (eval-after-load "diredful"
   '(diredful-settings-load))
 
