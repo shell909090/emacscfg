e13672a6dd5e960d87929f40ec5aafcc53f8a68d
Shell.Xu <shell909090@gmail.com>
2025-11-20 09:08:01 +0800

    remove warning

--- lisp/plugins.el
+++ lisp/plugins.el
@@ -91,8 +91,8 @@
  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
  python-shell-completion-setup-code
    "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
+ ;; python-shell-completion-module-string-code
+ ;;   "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code
    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 (defun electric-indent-ignore-python (char)
